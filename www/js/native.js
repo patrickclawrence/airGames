8cd10cef072dfc55ffb9e3c797b5d3ccf0d2d916
Patrick Lawrence <patrickl@airg.com>
2012-09-14 15:34:00 -0700

    Adding more js files and fixing a typo in the config file

---
+++ www/js/native.js
@@ -0,0 +1,60 @@
+
+function onDeviceReady(){
+	alert('the device is now ready!');
+}
+
+var native = {
+	TABS : ['NATIVE', 'AIRGAMES'],
+	currentTab: undefined,	
+	switchTabs: function (){
+		
+		window.plugins.childBrowser.showWebPage('http://www.airgames.com',
+            { 
+            	showLocationBar: false,
+            	showAddress: false,
+            	showNavigationbar: false
+        });	    	
+	},
+
+	bindEvents: function(){
+		//document.getElementById('native').onclick = native.switchTabs;
+		//document.getElementById('community').onclick = native.switchTabs;
+	},
+	switchTabs: function(tab){
+		console.log(tab)
+		if(tab != native.currentTab){			
+			native.currentTab = tab;
+			native.loadTab();
+		}
+	},
+	loadTab: function(){
+		switch (native.currentTab){
+			case native.TABS[0]:				
+				jQuery('#nativeContent').css('display', 'inline');
+				jQuery('#gamesContent').css('display', 'none');
+				break;
+			case native.TABS[1]:				
+				showWebPage("http://airgames.com", {showNavigationbar: false, showAddress: false, showWebPage: false, showLocationBar: false});
+				jQuery('#gamesContent').css('display', 'inline');
+				jQuery('#nativeContent').css('display', 'none');
+				break;
+			default:
+				console.log('You are now in content ' + native.currentTab);
+		}
+	}
+};
+
+document.addEventListener("deviceready", function(){
+	native.currentTab = native.TABS[0];
+	native.bindEvents();	
+}, false);
+/*
+jQuery(document).ready(function(){
+	native.currentTab = native.TABS[0];
+	native.bindEvents();	
+});
+*.
+
+
+
+
